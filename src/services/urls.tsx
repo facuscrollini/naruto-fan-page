1400efb0a0f147e92ef27cf01e8a42b128fb2ab1
facuscrollini <facuscrollinic@gmail.com>
2025-07-24 11:49:51 +0200

    Urls agregadas

---
+++ src/services/urls.tsx
@@ -0,0 +1,29 @@
+export const URL_GET_CHARACTERS = () => 'https://dattebayo-api.onrender.com/characters'
+export const URL_GET_CHARACTER = (id:number) => 'https://dattebayo-api.onrender.com/characters/' + id
+
+export const URL_GET_CLANS = () => 'https://dattebayo-api.onrender.com/clans'
+export const URL_GET_CLAN = (id:number) => 'https://dattebayo-api.onrender.com/clans' + id
+
+export const URL_GET_VILLAGES = () => 'https://dattebayo-api.onrender.com/villages'
+export const URL_GET_VILLAGE = (id:number) => 'https://dattebayo-api.onrender.com/villages' + id
+
+export const URL_GET_KEKKEIS = () => 'https://dattebayo-api.onrender.com/kekkei-genkai'
+export const URL_GET_KEKKEI = (id:number) => 'https://dattebayo-api.onrender.com/kekkei-genkai'+ id
+
+export const URL_GET_BEASTS = () => 'https://dattebayo-api.onrender.com/tailed-beasts'
+export const URL_GET_BEAST = (id: number) => 'https://dattebayo-api.onrender.com/tailed-beasts/' + id
+
+
+export const URL_GET_TEAMS = () => 'https://dattebayo-api.onrender.com/teams'
+export const URL_GET_TEAM = (id: number) => 'https://dattebayo-api.onrender.com/teams/' + id
+
+export const URL_GET_AKATSUKIS = () => 'https://dattebayo-api.onrender.com/akatsuki'
+export const URL_GET_AKATSUKI = (id: number) => 'https://dattebayo-api.onrender.com/akatsuki/' + id
+
+export const URL_GET_KARA = () => 'https://dattebayo-api.onrender.com/kara'
+export const URL_GET_KARAI = (id: number) => 'https://dattebayo-api.onrender.com/kara/' + id
+
+
+
+
+
