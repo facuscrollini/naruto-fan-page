6203379a425be21985eda3ce415d09cb70537e91
facuscrollini <facuscrollinic@gmail.com>
2025-08-18 14:36:27 +0200

    Simplificado el componente de WelcomeMessageImages

---
+++ src/components/home-components/welcome-message-components/WelcomeMessageImages.tsx
@@ -0,0 +1,19 @@
+
+export const WelcomeMessageImages = () => {
+  return (<>
+    <div className="absolute top-1/4 -left-1/6 -rotate-20 bg-amber-400 h-1/3 w-1/6 z-2 overflow-hidden rounded-xl border-3 border-black">
+    
+                    <img className="w-full object-cover h-full" src="https://i.pinimg.com/736x/10/d2/ec/10d2ec02b67ab4bcde0e6c085210d001.jpg" />
+                </div>
+                <div className="absolute top-2/4 -left-1/10 rotate-20 bg-amber-400 h-1/3 w-1/6 z-2 overflow-hidden rounded-xl border-3 border-black">
+                    <img className="w-full object-cover h-full" src="https://preview.redd.it/if-chakra-was-equal-for-the-tailed-beasts-son-goku-would-be-v0-zlz68g4bxmpd1.jpeg?width=640&crop=smart&auto=webp&s=5c467afa0a4a061768e53e9919f279ddba88ff2c" />
+                </div>
+                <div className="absolute top-1/4 -right-1/6 rotate-20 bg-amber-400 h-1/3 w-1/6 z-2 overflow-hidden rounded-xl border-2 border-black">
+                    <img className="w-full object-cover h-full" src="https://i.redd.it/what-is-this-style-of-art-coloring-called-v0-c53wj827x1xb1.jpg?width=1200&format=pjpg&auto=webp&s=c4a7d8802e09e820f687558c509480f600664870" />
+                </div>
+                <div className="absolute top-2/4 -right-1/20 -rotate-20 bg-amber-400 h-1/3 w-1/6 z-2 overflow-hidden rounded-xl border-2 border-black">
+                    <img className="w-full object-cover h-full" src="https://media.tenor.com/eLCmXURX2wAAAAAM/madara-uchiha.gif" />
+                </div>
+  </>
+  )
+}
