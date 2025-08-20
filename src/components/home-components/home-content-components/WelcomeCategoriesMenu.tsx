d494e7963f47249b785d65ef6ea2e5957e187598
facuscrollini <facuscrollinic@gmail.com>
2025-08-20 02:30:55 +0200

    Avanzando en logica de botones para elegir categoria en vista home

--- src/components/home-components/home-content-components/WelcomeCategoriesMenu.tsx
+++ src/components/home-components/home-content-components/WelcomeCategoriesMenu.tsx
@@ -5,30 +5,34 @@ export const WelcomeCategoriesMenu = () => {
 
   const { catalog, changeCatalog } = useAuth()
   const [position, setPosition] = useState<number>(3)
-  const [prevPosition, setPrevPosition] = useState(position -1)
-  const [nextPosition, setNextPosition] = useState(position + 2)
-  const elementos = ["A", "B", "C", "D", "E", "F"]
-  const [catalogCarrousel, setCatalogCarrousel] = useState(elementos.slice(prevPosition, nextPosition))
-  const swtichCatalog = (type: string, index: number) => {
+  const [carrousel, setCarrousel] = useState(listOfCatalogs.slice(2, 5))
 
+  const swtichCatalog = (type: string, index: number) => {
     catalog == type ? changeCatalog("") : changeCatalog(type)
-    position == index ? setPosition(7) : setPosition(index)
+    position == index ? setPosition(4) : setPosition(index)
+  }
+
+  const showTheIndex = (index: number) => {
+    console.log(index)
+  }
+
+  const decreasePosition = () =>{
+    setPosition(prev => position > 0 ? prev - 1 : 6)
+  }
+
+  const increasePosition = () =>{
+    setPosition(prev => position < 6 ? prev + 1 : 0)
   }
 
 
-
-
   useEffect(() => {
-    console.log(catalogCarrousel)
     position < 7 && changeCatalog(listOfCatalogs[position].type)
+    position == 6 ? setCarrousel([...listOfCatalogs.slice(-2), ...listOfCatalogs.slice(0, 1)]) : position == 0 ? setCarrousel([...listOfCatalogs.slice(-1), ...listOfCatalogs.slice(0, 2)]) : setCarrousel(listOfCatalogs.slice(position - 1, position + 2))
+    console.log("position desde el useEffect es " + position)
   }, [position])
 
 
-  useEffect(()=>{
-  //                     p-1  p p + 2
-  //                    0 , 1 , 2 ,3
-console.log(elementos.slice(0,3))
-  }, [])
+
 
   return (
     <div className=" text-center">
@@ -36,30 +40,26 @@ console.log(elementos.slice(0,3))
 
         <div className="grid 2xl:grid-cols-7 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 my-2">
 
-          {/* {
-            listOfCatalogs.map((element, index) => (
-              <div key={index} className={`relative transform  transition-all duration-500 ${catalog == element.type ? "md:scale-150 scale-110 md:translate-y-4 z-10" : "z-0"}`}>
+          {
+            carrousel.map((element, index) => (
+              <div key={index} className={`relative transform  transition-all duration-500 ${index == 1 ? "md:scale-150 scale-110 md:translate-y-4 z-10" : "z-0"}`}>
                 <button onClick={() => {
-
-                  swtichCatalog(element.type, index)
+                 swtichCatalog(element.type, index)
+                 index == 0 ? setPosition(prev => prev + 1) : index > 1 && setPosition(prev => prev -1) 
                 }
-                } className={`bg-gray-400 ${position != index ? "brightness-30" : "brightness-100"} rounded-lg py-0 px-3 w-full min-h-10 sm:min-w-50`}>{element.title}</button>
+                } className={`bg-gray-400 rounded-lg py-0 px-3 w-full min-h-10 sm:min-w-50`}>{element.title}</button>
               </div>
 
             ))
           }
-           */}
-          {
-           catalogCarrousel.map((elemento) => (
-              <div className="text-white">{elemento}</div>
-            ))
-          }
+
+
         </div>
       </div>
 
 
-      <button onClick={() => setPosition(prev => position > 0 ? prev - 1 : 6)} className="mr-2 hover:cursor-pointer hover:text-gray-900 text-gray-600 hover:scale-130 text-3xl transform transition-all duration-200"><i className="fa-solid fa-angle-left"></i></button>
-      <button onClick={() => setPosition(prev => position < 6 ? prev + 1 : 0)} className="ml-2 hover:cursor-pointer hover:text-gray-900 text-gray-600 hover:scale-130 text-3xl transform transition-all duration-200"><i className="fa-solid fa-angle-right"></i></button>
+      <button onClick={() => decreasePosition()} className="mr-2 hover:cursor-pointer hover:text-gray-900 text-gray-600 hover:scale-130 text-3xl transform transition-all duration-200"><i className="fa-solid fa-angle-left"></i></button>
+      <button onClick={() => decreasePosition()} className="ml-2 hover:cursor-pointer hover:text-gray-900 text-gray-600 hover:scale-130 text-3xl transform transition-all duration-200"><i className="fa-solid fa-angle-right"></i></button>
     </div>
 
 
--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -12,58 +12,58 @@ const initialState = {
 }
 
 export type UserType = {
-userName?:string;
-email?: string;
-password?:string,
+    userName?: string;
+    email?: string;
+    password?: string,
 }
 
 export type ValueType = {
     user?: UserType,
-    catalog?:string,
+    catalog?: string,
     logged: boolean,
-    changeCatalog: (type:string)=> void
-    login?: (username:string, email:string, password:string) => void,
-    logout?: ()=> void
+    changeCatalog: (type: string) => void
+    login?: (username: string, email: string, password: string) => void,
+    logout?: () => void
 }
 
 
 export const AuthContext = createContext<ValueType | undefined>(undefined)
 
 
-export const AuthProvider = ({children}:Props) =>{
+export const AuthProvider = ({ children }: Props) => {
 
-const [user, setUser] = useState<UserType>(initialState)
-const [catalog, setCatalog] = useState<string>("")
-const [logged, setLogged] = useState<boolean>(false)
+    const [user, setUser] = useState<UserType>(initialState)
+    const [catalog, setCatalog] = useState<string>("")
+    const [logged, setLogged] = useState<boolean>(false)
 
-const login = (userName:string, email:string, password:string)=>{
-setUser({
-   userName: userName,
-   email: email,
-   password: password
+    const login = (userName: string, email: string, password: string) => {
+        setUser({
+            userName: userName,
+            email: email,
+            password: password
 
-})
-setLogged(true)
-}
+        })
+        setLogged(true)
+    }
 
-const logout = () =>{
-    setUser({
-        userName: "",
-        email: "",
-        password: ""
-    })
-    setLogged(false)
-}
+    const logout = () => {
+        setUser({
+            userName: "",
+            email: "",
+            password: ""
+        })
+        setLogged(false)
+    }
 
-const changeCatalog = (type: string) =>{
-     setCatalog(type)
-}
+    const changeCatalog = (type: string) => {
+        setCatalog(type)
+    }
 
-return(
-<AuthContext.Provider value={{user,catalog, logged, changeCatalog, login, logout}}>
-    {children}
-</AuthContext.Provider>
-    
-)
+    return (
+        <AuthContext.Provider value={{ user, catalog, logged, changeCatalog, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+
+    )
 
 }
