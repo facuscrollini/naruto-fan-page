5a20d5151c95c661951366e940b66acec7fe98ff
facuscrollini <facuscrollinic@gmail.com>
2025-09-03 13:55:58 +0200

    Agregada la funcionalidad en el componente CatalogCardGroup para que maneje las imagenes que no tiene la api para las villas

--- src/components/auth-components/catalog-components/catalog-body-components/catalog-list-components/catalog-list-body-components/CatalogCardGroup.tsx
+++ src/components/auth-components/catalog-components/catalog-body-components/catalog-list-components/catalog-list-body-components/CatalogCardGroup.tsx
@@ -13,29 +13,44 @@ type Props = {
 
 export const CatalogCardGroup = ({ page, setPage, setQuantityOfPages }: Props) => {
 
- //I create an array that contains all images for villages by objects
- const villages = [
-  {id:0, image: ""},
-  {id:1},
-  //no image id 2
-  {id:2},
-  {id:3},
-  //no image id 4
-  {id:4},
-  {id:5},
-  {id:6},
-  {id:7},
-  {id:8},
-  //no image id 9
-  {id:9},
-  {id:10},
-  {id:11},
-  {id:12},
-  {id:13},
-  {id:14},
-  //no image id 15
-  {id:15},
- ]
+  //I create an array that contains all images for villages by objects
+  const villages = [
+    { id: 0, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756899385/1000_muunvc.png" },
+    { id: 1, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809102/1000_j2frzo.png" },
+    //no image id 
+    { id: 2, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809451/2878_f5ds8x.png" },
+    { id: 3, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809468/1000_s5vvdf.png" },
+    //no image id 4
+    { id: 4, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809451/2878_f5ds8x.png" },
+    { id: 5, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756899008/1000_qwulmi.png" },
+    { id: 6, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809736/1000_dyx590.png" },
+    { id: 7, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809795/1000_jlgqu1.png" },
+    { id: 8, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809859/259_xs8924.png" },
+    //no image id 9
+    { id: 9, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809859/259_xs8924.png" },
+    { id: 10, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809984/latest_imlh50.png" },
+    { id: 11, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756810004/1000_kisyg1.jpg" },
+    { id: 12, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756810098/1000_ffvyoz.png" },
+    { id: 13, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756810152/latest_fjkr9i.png" },
+    { id: 14, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756810174/latest_xw5ngo.png" },
+    //no image id 15
+    { id: 15, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809451/2878_f5ds8x.png" },
+    { id: 16, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756899566/latest_zz2wi9.png" },
+    { id: 17, image: "" },
+    { id: 18, image: "" },
+    { id: 19, image: "" },
+    { id: 20, image: "" },
+    { id: 21, image: "" },
+    { id: 22, image: "" },
+    { id: 23, image: "" },
+    { id: 24, image: "" },
+    { id: 25, image: "" },
+    { id: 26, image: "" },
+    { id: 27, image: "" },
+    { id: 28, image: "" },
+    { id: 29, image: "" },
+    { id: 30, image: "" }
+  ]
   const quantityPerPage = 16
 
   const { store } = useNaruto()
@@ -68,16 +83,20 @@ export const CatalogCardGroup = ({ page, setPage, setQuantityOfPages }: Props) =
 
 
   //I'm gonna use the useEffect to show all the listItems items everytime it changes
-//There are 15 positions on village catalog
-  useEffect(()=>{console.log(listItems)},[listItems])
+  //There are 15 positions on village catalog
+  useEffect(() => { console.log(listItems) }, [listItems])
 
 
   return (
     <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 my-[2rem] gap-2 mx-[2rem] h-[45vh] overflow-auto">
-      {listItems && listItems.map((item, index) => (
+      {listItems && listItems.map((item, index) => {
       
-        <CatalogCard key={index} image={"images" in item ? item.images[0] : "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756807956/1000_r0ow0d.png"} name={item.name} />
-      ))}
+      const image = "images" in item 
+      ? item.images[0]
+      : (villages.find(el => el.id == item.id)?.image)
+
+        return <CatalogCard key={index} image={image} name={item.name} />
+      })}
 
     </div>
   )
