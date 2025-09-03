df4fcb6d560fec87c37f73d58090850c8eaa2387
facuscrollini <facuscrollinic@gmail.com>
2025-09-03 14:34:41 +0200

    Agregadas todas las imágenes a cada villa

--- src/components/auth-components/catalog-components/catalog-body-components/catalog-list-components/catalog-list-body-components/CatalogCardGroup.tsx
+++ src/components/auth-components/catalog-components/catalog-body-components/catalog-list-components/catalog-list-body-components/CatalogCardGroup.tsx
@@ -35,21 +35,32 @@ export const CatalogCardGroup = ({ page, setPage, setQuantityOfPages }: Props) =
     { id: 14, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756810174/latest_xw5ngo.png" },
     //no image id 15
     { id: 15, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809451/2878_f5ds8x.png" },
+
     { id: 16, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756899566/latest_zz2wi9.png" },
-    { id: 17, image: "" },
-    { id: 18, image: "" },
-    { id: 19, image: "" },
-    { id: 20, image: "" },
-    { id: 21, image: "" },
-    { id: 22, image: "" },
-    { id: 23, image: "" },
-    { id: 24, image: "" },
-    { id: 25, image: "" },
-    { id: 26, image: "" },
-    { id: 27, image: "" },
-    { id: 28, image: "" },
-    { id: 29, image: "" },
-    { id: 30, image: "" }
+    { id: 17, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900773/1000_xlh3tn.png" },
+    { id: 18, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900786/1000_n6owoz.png" },
+    { id: 19, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900835/latest_kqzflh.png" },
+    { id: 20, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900884/320_x0pth7.png" },
+    { id: 21, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901097/320_nfxrke.png" },
+    { id: 22, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900986/1000_rzydbu.png" },
+    { id: 23, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756900978/1000_jvgbbr.png" },
+    { id: 24, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901328/latest_kwvjdf.jpg" },
+    { id: 25, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901483/dec98-16651481780318_cgt9ju.jpg" },
+    { id: 26, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756809451/2878_f5ds8x.png" },
+    { id: 27, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901805/1000_hxcjja.jpg" },
+    { id: 28, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901888/1000_ecccxi.png" },
+    { id: 29, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901939/1000_duzyh1.png" },
+    { id: 30, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756901965/1000_un0oms.png" },
+
+    { id: 31, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902508/223_cfkeoh.jpg" },
+    { id: 32, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902033/1000_rbpwig.png" },
+    { id: 33, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902056/1000_qakqqg.png" },
+    { id: 34, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902550/latest_x7ljsa.jpg" },
+    { id: 35, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902594/1000_hscqyg.png" },
+    { id: 36, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902645/1000_i3ku7y.png" },
+    { id: 37, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902721/latest_xjjfvt.jpg" },
+    { id: 38, image: "https://res.cloudinary.com/dra2cr3uw/image/upload/v1756902791/latest_jnhfbg.png" }
+
   ]
   const quantityPerPage = 16
 
@@ -90,10 +101,10 @@ export const CatalogCardGroup = ({ page, setPage, setQuantityOfPages }: Props) =
   return (
     <div className="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 my-[2rem] gap-2 mx-[2rem] h-[45vh] overflow-auto">
       {listItems && listItems.map((item, index) => {
-      
-      const image = "images" in item 
-      ? item.images[0]
-      : (villages.find(el => el.id == item.id)?.image)
+
+        const image = "images" in item
+          ? item.images[0]
+          : (villages.find(el => el.id == item.id)?.image)
 
         return <CatalogCard key={index} image={image} name={item.name} />
       })}
